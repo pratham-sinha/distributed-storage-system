23edf1da5f441715bde5daf4ef4496087868503e
Pratham Sinha <prathamsinha7777@gmail.com>
2026-08-07 16:59:18 +0530

    Initialized coordinator and storage-node's spring boot services

---
+++ coordinator/src/test/java/com/distributedstorage/coordinator/CoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedstorage.coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
