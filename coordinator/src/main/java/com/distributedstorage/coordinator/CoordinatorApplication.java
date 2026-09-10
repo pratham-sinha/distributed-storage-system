363802833db2296c3e446e37a85bfaf475d58020
Kumar Anshuman <kanshuman6484@gmail.com>
2026-09-10 20:16:25 +0530

    create: db connection

--- coordinator/src/main/java/com/distributedstorage/coordinator/CoordinatorApplication.java
+++ coordinator/src/main/java/com/distributedstorage/coordinator/CoordinatorApplication.java
@@ -2,6 +2,9 @@ package com.distributedstorage.coordinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoordinatorApplication {
@@ -9,5 +12,10 @@ public class CoordinatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoordinatorApplication.class, args);
     }
+
+    @Bean
+	ApplicationListener<ApplicationReadyEvent> ready() {
+		return event -> System.out.println("\n✅ Server is ready!\n");
+	}
 
 }
